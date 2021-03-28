34276af3630d7d58df4187d3b2d450e7d02772eb
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2021-03-28 15:55:03 -0400

    Adding changes

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,121 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from "react";
+import styles from "./components.module.css";
+import SpotifyWebApi from "spotify-web-api-js";
+const spotifyApi = new SpotifyWebApi();
 
-export default function Home() {
+export default function Login() {
+  var params = null;
+  var token = null;
+  if (typeof window !== "undefined") {
+    params = getHashParams();
+    token = params.access_token;
+  }
+
+  const [loggedIn, setLoggedIn] = useState(token ? true : false);
+  const [nowPlaying, setPlaying] = useState({
+    time: 0,
+    duration: 0,
+    name: "Not Playing",
+    albumArt: "",
+    artist: [],
+    albumName: "",
+  });
+  if (token) {
+    spotifyApi.setAccessToken(token);
+  }
+
+  function getPlaying() {
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      setPlaying({
+        time: response.progress_ms,
+        duration: response.item.duration_ms,
+        name: response.item.name,
+        albumArt: response.item.album.images[0].url,
+        artist: response.item.artists,
+        albumName: response.item.album.name,
+      });
+    });
+  }
+
+  // function getAnalysis(){
+  //   spotifyApi.getAudioAnalysisForTrack().then
+  // }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(loggedIn){
+        getPlaying();
+      }
+      
+    }, 500);
+    return () => clearInterval(interval);
+  }, []);
+
+  function getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div>
+      {loggedIn && nowPlaying ? (
+        <div>
+        <div className={styles.container}>
+          <div className={styles.songInfo}>
+            <div className={styles.songDesc}>
+              <img src={nowPlaying.albumArt} style={{ height: 275 }} />
+              <div className={styles.songDetails}>
+                <div>
+                  <h1>{nowPlaying.name}</h1>
+                  <h2>
+                    {nowPlaying.artist.map(({ name }, i) => (
+                      <span key={i}>
+                        {name}
+                        {nowPlaying.artist.length > 0 &&
+                        i === nowPlaying.artist.length - 1
+                          ? ""
+                          : ", "}
+                      </span>
+                    ))}
+                  </h2>
+                  <h3>{nowPlaying.albumName}</h3>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className={styles.progress}>
+                <div
+                  className={styles.progress__bar}
+                  style={{
+                    width: `${(nowPlaying.time * 100) / nowPlaying.duration}%`,
+                  }}
+                ></div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p>test</p>
+          </div>
+          <div>
+            <p>test</p>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </div>
+      ) : (
+        <div className={styles.login}>
+        <a className={styles.button} href="http://localhost:8888/login">
+          Login with Spotify
         </a>
-      </footer>
+        </div>
+      )}
     </div>
-  )
+  );
 }
